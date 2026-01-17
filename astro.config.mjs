4c2459d4150f2faabbd7b7194d11660f5dafd50f
tejasms03 <f20211415@pilani.bits-pilani.ac.in>
2026-01-16 19:50:21 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://tejasms03.github.io",
+  base: "./",          // VERY IMPORTANT: relative paths
+  output: "static",
   vite: {
+    base: "./",        // relative paths for _astro
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
 });
